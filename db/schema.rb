82c367ccf14993d6571f0dbaf54ac0475b914882
lbias <306508354@qq.com>
2017-05-12 17:58:22 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512095030) do
+ActiveRecord::Schema.define(version: 20170512095606) do
+
+  create_table "course_relationships", force: :cascade do |t|
+    t.integer  "course_id"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +27,13 @@ ActiveRecord::Schema.define(version: 20170512095030) do
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teacher_relationships", force: :cascade do |t|
+    t.integer  "teacher_id"
+    t.integer  "student_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
