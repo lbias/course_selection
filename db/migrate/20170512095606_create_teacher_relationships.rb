2dddcd8d06ca8d8fe1d1e7cd63b2df3a6570e2be
lbias <306508354@qq.com>
2017-05-12 17:57:29 +0800

    add teacher_id, student_id to table teacher_relationship

--- db/migrate/20170512095606_create_teacher_relationships.rb
+++ db/migrate/20170512095606_create_teacher_relationships.rb
@@ -1,7 +1,8 @@
 class CreateTeacherRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :teacher_relationships do |t|
-
+      t.integer :teacher_id
+      t.integer :student_id
       t.timestamps
     end
   end
