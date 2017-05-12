a0a439b4908a32cf4d88339491acafbf44d91f22
lbias <306508354@qq.com>
2017-05-12 18:08:48 +0800

    association between course and teacher

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
   has_many :course_relationships
   has_many :students, through: :course_relationships, source: :student
+  belongs_to :teacher #one course can only be teached by one teacher, one teacher can teacher many courses
 end
